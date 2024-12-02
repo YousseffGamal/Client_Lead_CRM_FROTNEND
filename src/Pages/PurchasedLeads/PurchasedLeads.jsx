de1105f3975b696e31048b657fda4b12a1a44f3c
Hanin Khairy <hanindahb007@gmail.com>
2024-12-02 15:16:45 +0200

    bided purchased leads

--- src/Pages/PurchasedLeads/PurchasedLeads.jsx
+++ src/Pages/PurchasedLeads/PurchasedLeads.jsx
@@ -1,270 +1,58 @@
-import React, { useState, useEffect, useMemo } from "react";
-import { Box, Modal } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Box } from "@mui/material";
 import Layout from "../../component/Layout/Layout";
 import LeadCard from "../../component/LeadCard/LeadCard";
 import PiddingCard from "../../component/PiddingCard/PiddingCard";
 import SwitchComponent from "../../component/SwitchComponent/SwitchComponent";
-import FilterComponent from "../../component/FilterComponent/FilterComponent"; // Adjust the path if necessary
+import FilterComponent from "../../component/FilterComponent/FilterComponent";
 
 import { useAuth } from "../../store/authContext";
 import axiosInstance from "../../axios";
-import ChildModal from "../../component/childModal/ChildModal";
 
-// Sample leads data
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
-};
 const PurchasedLeads = () => {
-  const [activeTab, setActiveTab] = useState(0); // State to track which card to display
-  const [pricedLeads, setpricedLeads] = useState([]); // State for filtered leads
-  const [biddingLeads, setbiddingLeads] = useState([]); // State for filtered leads
-  const [isBidding, setIsBidding] = useState(false); // Toggle state
+  const [activeTab, setActiveTab] = useState(0); 
+  const [pricedLeads, setPricedLeads] = useState([]); 
+  const [biddingLeads, setBiddingLeads] = useState([]); 
+  const { auth } = useAuth(); 
 
-  const [error, setError] = useState(null);
-  const [errorMessage, setErrorMessage] = useState("error");
-  const [clientSecret, setClientSecret] = useState("");
-  const [customerId, setCustomerId] = useState("");
-
-  const { auth, setAuth } = useAuth();
-  const [verified, setVerified] = useState(false);
-  const memoizedAuth = useMemo(() => auth, [auth]);
-  useEffect(() => {
-    setVerified(memoizedAuth.user.paymentMethod);
-
-    console.log("Auth changed:", memoizedAuth);
-  }, [memoizedAuth]);
-
-  const [bidAmount, setbidAmount] = useState("");
-  const [bidDuration, setBidDuration] = useState(0);
-
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = () => {
-    axiosInstance
-      .post("setupPaymentIntent", {
-        email: auth.user.email,
-        name: auth.user.firstName,
-      })
-      .then((res) => {
-        setClientSecret(res.data.clientSecret);
-        setCustomerId(res.data.customerId);
-        setOpen(true);
-      })
-      .catch((err) => {
-        console.log("Error fetching client secret:", err);
-        return;
-      });
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const modalClose = () => {
-    getbiddingLeads();
-    setAuth({ ...auth, paymentMethod: true });
-    console.log("cameHereModalClose");
-    localStorage.setItem("paymentMethod", true);
-    setActiveTab(1);
-
-    setOpen(false);
-  };
-
-  let ws; // Declare the WebSocket instance
-
-  useEffect(() => {
-    // Initialize the WebSocket connection
-    ws = new WebSocket("http://localhost:8080");
-
-    // Event listener for when the connection is open
-    ws.onopen = () => {
-      console.log("Connected to WebSocket");
-      // Send an initial message, if needed
-      ws.send("Hello Server!");
-    };
-
-    // Event listener for incoming messages
-    ws.onmessage = async (event) => {
-      console.log("Message from server:", event.data);
-      // Handle the case where the event data is already JSON
-      try {
-        const data = JSON.parse(event.data);
-        if (data.isBidding === true) {
-          setbiddingLeads((biddingLeads) => [...biddingLeads, data]);
-        } else if (data.isBidding === false) {
-          setpricedLeads((pricedLeads) => [...pricedLeads, data]);
-        } else if (data.savedBid) {
-          setBidDuration(data.duration);
-          setbiddingLeads((prevItems) =>
-            prevItems.map((item) => {
-              if (item._id === data.savedBid.Lead) {
-                const updatedBid = {
-                  ...data.savedBid,
-                  BidDurationDelay: data.duration, // Example: Adding a new property
-                };
-
-                return {
-                  ...item,
-                  bids: [updatedBid, ...item.bids],
-                  BidDurationDelay: data.duration,
-                  // initialBiddingPrice: res.data.bid.bidAmount,
-                };
-              }
-              return item;
-            })
-          );
-        } else if (data.name === "scheduleLeadPurchaseJob") {
-          setbiddingLeads((prevItems) =>
-            prevItems.map((item) =>
-              item._id === data.leadId ? { ...item, status: "Open" } : item
-            )
-          );
-        } else if (data.name === "closeLead") {
-          setbiddingLeads((prevItems) =>
-            prevItems.map((item) =>
-              item._id === data.leadId ? { ...item, status: "Closed" } : item
-            )
-          );
-        }
-      } catch (error) {
-        console.error("Failed to parse JSON:", error);
-      }
-    };
-    //yzhr mn 8ir arkam
-    //aftergetting the lead - comment & schedule mail message (task)
-    //payment
-    // Event listener for errors
-    ws.onerror = (error) => {
-      console.error("WebSocket Error:", error);
-    };
-
-    // Event listener for when the connection is closed
-    ws.onclose = () => {
-      console.log("WebSocket connection closed");
-    };
-
-    // Clean up the WebSocket connection when the component unmounts
-    return () => {
-      ws.close();
-    };
-  }, []);
-
-
+  
   const fetchLeads = async () => {
     try {
-      console.log("auth", auth.user._id);
-      const endpoint = isBidding
-        ? `/userBiddingLeads/${auth.user._id}` // API for bidding leads
-        : `/userPurchasedLeads/${auth.user._id}`; // API for normal purchased leads
+      const endpoint =
+        activeTab === 0
+          ? `/userPurchasedLeads/${auth.user._id}` // API for purchased leads
+          : `/winnerleads/${auth.user._id}`; // API for bidding leads
+  
+      console.log("Request URL:", endpoint); // Debug log the request URL
+  
       const response = await axiosInstance.get(endpoint);
-      setpricedLeads(response.data.data);
+      console.log("API Response:", response);
+  
+      if (activeTab === 0) {
+        setPricedLeads(response.data.data);
+      } else {
+        setBiddingLeads(response.data.data);
+      }
     } catch (err) {
-      console.log(err);
+      console.error("Error fetching leads:", err); // Log any error that occurs during the fetch
     }
   };
   
-
-  // Fetch leads from API
+  
+  // Fetch data when the active tab changes
   useEffect(() => {
     fetchLeads();
-  }, []);
-  const getbiddingLeads = () => {
-    axiosInstance
-      .get("biddingLeads")
-      .then((res) => {
-        setbiddingLeads(res.data.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+  }, [activeTab]);
+
+  // Handle tab switch
   const switchView = (event) => {
-    if (auth.paymentMethod) {
-      getbiddingLeads();
-      setActiveTab(event.target.checked ? 1 : 0);
-    } else {
-      console.log(auth);
-      handleOpen();
-    }
-  };
-
-  const handleBidChange = (leadId, value) => {
-    setbiddingLeads((prevItems) =>
-      prevItems.map((item) =>
-        item._id === leadId ? { ...item, value: value } : item
-      )
-    );
-    setbidAmount(value);
-  };
-
-  const biddingAmount = (Lead) => {
-    const lead = biddingLeads.find((item) => item._id === Lead);
-    axiosInstance
-      .post("/bids", {
-        bidderId: auth.user._id,
-        bidAmount: lead.value,
-        Lead,
-      })
-      .then((res) => {
-        setbiddingLeads((prevItems) =>
-          prevItems.map((item) =>
-            item._id === Lead
-              ? {
-                  ...item,
-                  value: "",
-                  error: "",
-                  bids: [res.data.bid, ...item.bids],
-                  // intialBiddingPrice: res.data.bid.bidAmount,
-                }
-              : item
-          )
-        );
-
-        // setbidAmount("");
-      })
-      .catch((err) => {
-        setErrorMessage(err.response.data.message);
-        setbiddingLeads((prevItems) =>
-          prevItems.map((item) =>
-            item._id === Lead
-              ? { ...item, value: "", error: err.response.data.message }
-              : item
-          )
-        );
-      });
+    setActiveTab(event.target.checked ? 1 : 0);
   };
 
   return (
     <Layout>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="parent-modal-title"
-        aria-describedby="parent-modal-description"
-      >
-        <Box sx={{ ...style, width: 400 }}>
-          <h2 id="parent-modal-title">You need payment verification</h2>
-          <p id="parent-modal-description">
-            You need to verify your payment first to access bidding leads
-          </p>
-          <ChildModal
-            clientSecret={clientSecret}
-            customerId={customerId}
-            close={modalClose}
-          />
-        </Box>
-      </Modal>
       <Box sx={{ p: 3, backgroundColor: "#F1F1F1", marginTop: "65px" }}>
         {/* Filter and Switch Components */}
-
         <Box
           sx={{
             display: "flex",
@@ -280,13 +68,10 @@ const PurchasedLeads = () => {
           }}
         >
           <SwitchComponent activeTab={activeTab} switchView={switchView} />
-          <FilterComponent
-            setpricedLeads={setpricedLeads}
-            sx={{ ml: 1 }}
-          />{" "}
-          {/* Add left margin if needed */}
+          <FilterComponent setPricedLeads={setPricedLeads} sx={{ ml: 1 }} />
         </Box>
 
+        {/* Cards Display */}
         <Box
           sx={{
             display: "flex",
@@ -294,74 +79,70 @@ const PurchasedLeads = () => {
             justifyContent: "center",
           }}
         >
-          {activeTab === 0
-            ? pricedLeads.map((lead, index) => (
-                <Box
-                  key={index}
-                  sx={{
-                    width: {
-                      xs: "100%", // Full width on small screens
-                      sm: "50%", // Two cards per row on small screens
-                      md: "33.33%", // Three cards per row on medium screens
-                      lg: "33.33%", // Three cards per row on large screens
-                    },
-                    padding: 1,
-                    boxSizing: "border-box",
-                  }}
-                >
-                  <LeadCard
-                          leadId={lead._id} // Ensure this is passed
-                          address={lead.addressLine}
-                          city={lead.county.name}
-                          condition={lead.condition}
-                          askingPrice={lead.askingPrice}
-                          leadType={lead.leadType?.name}
-                          closingTime={lead.closingTime}
-                          occupancy={lead.occupancy}
-                  />
-                </Box>
-              ))
-            : biddingLeads.map((lead, index) => (
-                <Box
-                  key={index}
-                  sx={{
-                    width: {
-                      xs: "100%", // Full width on small screens
-                      sm: "50%", // Two cards per row on small and up
-                      md: activeTab === 0 ? "33.33%" : "50%", // For LeadCard, three cards in medium screens, and two for PiddingCard
-                      lg: activeTab === 0 ? "25%" : "50%", // Four cards in large screens for LeadCard, two for PiddingCard
-                    },
-                    padding: 1,
-                    boxSizing: "border-box",
-                  }}
-                >
-                  {
-                    <PiddingCard
-                      lead={lead}
-                      leadId={lead._id}
-                      address={lead.addressLine}
-                      city={lead.county.name}
-                      condition={lead.condition}
-                      intialBiddingPrice={
-                        lead.bids.length != 0
-                          ? lead.bids[0].bidAmount
-                          : lead.intialBiddingPrice
-                      }
-                      BidDurationDelay={lead.BidDurationDelay}
-                      leadType={lead.leadType?.name}
-                      closingTime={lead.closingTime}
-                      occupancy={lead.occupancy}
-                      status={lead.status}
-                      biddingAmount={biddingAmount}
-                      setbidAmount={setbidAmount}
-                      bidAmount={bidAmount}
-                      value={lead.value}
-                      errorMessage={lead.error}
-                      onBidChange={handleBidChange}
-                    />
+          {/* Render purchased leads */}
+          {activeTab === 0 &&
+            pricedLeads.map((lead, index) => (
+              <Box
+                key={index}
+                sx={{
+                  width: {
+                    xs: "100%", // Full width on small screens
+                    sm: "50%", // Two cards per row on small screens
+                    md: "33.33%", // Three cards per row on medium screens
+                    lg: "33.33%", // Three cards per row on large screens
+                  },
+                  padding: 1,
+                  boxSizing: "border-box",
+                }}
+              >
+                <LeadCard
+                  leadId={lead._id}
+                  address={lead.addressLine}
+                  city={lead.county?.name || "N/A"} 
+                  condition={lead.condition}
+                  askingPrice={lead.askingPrice}
+                  leadType={lead.leadType?.name || "N/A"}
+                  closingTime={lead.closingTime}
+                  occupancy={lead.occupancy}
+                />
+              </Box>
+            ))}
+
+          {/* Render bidding leads */}
+          {activeTab === 1 &&
+            biddingLeads.map((lead, index) => (
+              <Box
+                key={index}
+                sx={{
+                  width: {
+                    xs: "100%", // Full width on small screens
+                    sm: "50%", // Two cards per row on small screens
+                    md: "33.33%", // Three cards per row on medium screens
+                    lg: "33.33%", // Three cards per row on large screens
+                  },
+                  padding: 1,
+                  boxSizing: "border-box",
+                }}
+              >
+                <PiddingCard
+                  lead={lead}
+                  leadId={lead._id}
+                  address={lead.addressLine}
+                  city={lead.county?.name || "N/A"}
+                  condition={lead.condition}
+                  intialBiddingPrice={
+                    lead.bids.length !== 0
+                      ? lead.bids[0].bidAmount
+                      : lead.intialBiddingPrice
                   }
-                </Box>
-              ))}
+                  BidDurationDelay={lead.BidDurationDelay}
+                  leadType={lead.leadType?.name || "N/A"}
+                  closingTime={lead.closingTime}
+                  occupancy={lead.occupancy}
+                  status={lead.status}
+                />
+              </Box>
+            ))}
         </Box>
       </Box>
     </Layout>
